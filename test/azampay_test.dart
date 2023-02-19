1c931086d970363b272f16eb8fe821fe2f07ca9a
kalokola <brightiuskalokola@gmail.com>
2023-02-20 00:20:40 +0300

    AzamPay

--- test/azampay_test.dart
+++ test/azampay_test.dart
@@ -3,5 +3,5 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:azampay/azampay.dart';
 
 void main() {
-  // Will be available on azampay: 0.0.2
+  // Will be available on azampay: ^0.0.2
 }
